c02d949483411a65f18d388779de5cf3dd34b61c
neokarn <34811101+neokarn@users.noreply.github.com>
2021-01-14 20:16:26 +0700

    Update lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,24 +1,9 @@
 #include<iostream>
-
 using namespace std;
 
-//Write prototype of function fibonacci() here
-
 int main(){
-	cout << fibonacci(0) << "\n";
-	cout << fibonacci(1) << "\n";
-	cout << fibonacci(2) << "\n";
-	cout << fibonacci(15) << "\n";
-	cout << fibonacci(30) << "\n";
-	cout << fibonacci(50) << "\n";
-	
-	return 0;
+    int x;
+    cin >> x;
+    cout << fibonacci(x);
+    return 0; 
 }
-
-
-//Write definition of function fibonacci() here
-/*
-??? fibonacci(???){
-  
-}
-*/
