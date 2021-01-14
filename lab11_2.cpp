73ed5b9e910c1332ba614d7bd8f5548656550d5a
neokarn <34811101+neokarn@users.noreply.github.com>
2021-01-14 20:18:41 +0700

    Update lab11_2.cpp

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -1,23 +1,9 @@
 #include<iostream>
-
 using namespace std;
 
-//Write function prototype here
-
 int main(){
-	cout << gcd(25,15) << "\n";
-	cout << gcd(144,60) << "\n";
-	cout << gcd(60,144) << "\n";
-	cout << gcd(1,69) << "\n";
-	cout << gcd(17,19) << "\n";
-	cout << gcd(51,255) << "\n";
-	
-	return 0;
+    int x, y;
+    cin >> x >> y;
+    cout << gcd(x,y);
+    return 0; 
 }
-
-//Write function definition here
-/*
-??? gcd(???){
-
-}
-*/
