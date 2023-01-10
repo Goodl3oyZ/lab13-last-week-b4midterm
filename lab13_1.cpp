e38f27830e5581d040bf2756dfec39787d0f15d5
Goodl3oyZ <panudetzx2@gmail.com>
2023-01-10 15:08:25 +0700

    13.1 come on

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
-
+int fibonacci(int);
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
 }
+int fibonacci(int a)
+{
+    if(a==0)
+        return 0;
+    else if(a==1)
+        return 1;
+    else
+        return (fibonacci(a-1) + fibonacci(a-2));
+}
+
+
