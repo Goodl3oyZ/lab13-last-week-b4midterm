a6430b2df40db8196536b14ff234644a15bf325b
Goodl3oyZ <panudetzx2@gmail.com>
2023-01-10 15:25:20 +0700

    13.2 gonext

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
+#include <ctime>
 using namespace std;
-
+long long int fibonacci(long long int);
 int main(){
-    int x, y;
-    cin >> x >> y;
-    cout << gcd(x,y);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+}
+long long int fibonacci(long long int a)
+{
+    if(a==0)
+        return 0;
+    else if(a==1)
+        return 1;
+    else
+        return (fibonacci(a-1) + fibonacci(a-2));
 }
