77ef2303d43f8e6f05f4fa6e6695619760a4b710
Goodl3oyZ <panudetzx2@gmail.com>
2023-01-10 15:58:37 +0700

    so hard last one

--- lab13_4.cpp
+++ lab13_4.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
-
+#include <string>
 using namespace std;
+
+template <typename s>
+void mySwap(s   &, s    &);
 
 int main(){
 	int x, y;
@@ -30,3 +33,11 @@ int main(){
 	return 0;
 }
 
+template <typename swap>
+void mySwap(swap &c,swap &d)
+{
+    swap x=c;
+     c=d;
+     d=x;
+
+}
